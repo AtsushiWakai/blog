c31fca02ef6a2269a8057805bd1868e51dc3d5cd
AtsushiWakai <wakai.atsushi@members.co.jp>
2019-06-11 07:57:42 +0900

    firstcommit

---
+++ py_webapp/py_webapp/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\stf01290\documents\git\tool_jcb\tools_jcb\py_webapp\py_webapp\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
